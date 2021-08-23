53dbf377695c3194f53f8b1ad71215c42ab40ee8
immaxvel <immaxvel@gmail.com>
2021-08-23 12:54:35 +0300

    redirect

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from "./Dialogs.module.css"
 import DialogItem from "./DialogsItem/DialogsItem";
 import Message from "./Message/Message";
+import {Redirect} from "react-router-dom";
 
 const Dialogs = (props) => {
     let state = props.dialogsPage;
 
     let dialogsElements = state.dialogs.map(dialog => <DialogItem key={dialog.id} name={dialog.name}
-                                                                        id={dialog.name}/>);
+                                                                  id={dialog.name}/>);
     let messagesElements = state.messages.map(message => <Message key={message.id} message={message.message}/>);
 
     let newMessageBody = state.newMessageBody;
@@ -16,10 +17,10 @@ const Dialogs = (props) => {
         props.sendMessage();
     }
     let onNewMessageChange = (e) => {
-       let body =  e.target.value;
-       props.updateNewMessageBody(body);
+        let body = e.target.value;
+        props.updateNewMessageBody(body);
     }
-
+    if(!props.isAuth) return <Redirect to={'/login'}/>;
     return (
         <div>
             <div className={styles.dialogs}>
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = (props) => {
+    return (
+        <h1>
+            Please LOG IN!
+        </h1>
+    )
+}
+
+export default Login;
