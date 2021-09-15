98b0ee0723e591887015fbbc116dd92d6948a26e
immaxvel <Max931716>
2021-09-15 13:47:30 +0300

    login form redux

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
 import styles from './Login.module.css'
-
-const Login = (props) => {
-    return (
-        <div className={styles.login}>
-            <h1> Please LOG IN! </h1>
-            <LoginForm/>
-        </div>
-    )
-}
-
+import {Field, reduxForm} from "redux-form";
 
 const LoginForm = (props) => {
     return (
-        <form>
+        <form onSubmit={props.handleSubmit}>
             <div>
-                <input placeholder={"Login"}/>
+                <Field placeholder={"Login"} name={'login'} component={'input'}/>
             </div>
             <div>
-                <input placeholder={"Password"}/>
+                <Field placeholder={"Password"} name={'password'} component={'input'}/>
             </div>
             <div>
-                <input type={"Checkbox"}/> Remember me
+                <Field type={"Checkbox"} name={'rememberMe'} component={'input'}/> Remember me
             </div>
             <div>
                 <button>Login</button>
@@ -30,5 +21,21 @@ const LoginForm = (props) => {
 
     )
 }
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+const Login = (props) => {
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return (
+        <div className={styles.login}>
+            <h1> Please LOG IN! </h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    )
+}
+
 
 export default Login;
